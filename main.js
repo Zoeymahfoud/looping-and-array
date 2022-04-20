4dfa251515466f376d3cb06deee213be4423c011
Zoeymahfoud <98282440+Zoeymahfoud@users.noreply.github.com>
2022-04-20 11:50:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+for(let i = 2500; i < 10000; i++) {
+  if ((i % 2) == 0) {
+  alert("A quarter of the way there!"); i= 2500
+  console.log(i);  
+  }
+}
+
+for(let i = 5000; i < 10000; i++) {
+  if ((i % 2) == 0) {
+  alert("Halfway there!"); i= 5000
+  console.log(i);  
+  }
+}
+for(let i = 10000; i < 10000; i++) {
+  if ((i % 2) == 0) {
+  alert("The loop is done!"); i= 5000
+  console.log(i);  
+  }
+}
+
+
+var shows = ['Stranget Things', 'Big Bang Theory', 'The Queens Gambit', 'Vampire Diaries', 'Supernatural'];
+// Fav TV shows
+
+console.log(shows);
+
+for(let i = 0; i < shows.length; i++) {
+  console.log(shows.lenght[i])
+ }
